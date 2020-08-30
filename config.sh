06f229f2f48d41a680ba8ea70e69f04fc574f99e
Frederic Depuydt <puydt7@gmail.com>
2020-08-31 01:22:24 +0200

    Converted for use with multiple repo's

--- config.sh
+++
@@ -1,6 +0,0 @@
-sed -i 's/^\( *\)- traefik-conf:\/etc\/traefik:ro *$/\1- traefik-conf:\/etc\/traefik/g' docker-compose.yml
-docker-compose up --no-start
-docker cp traefik.toml traefik:/etc/traefik
-docker cp toml/ traefik:/etc/traefik
-sed -i 's/^\( *\)- traefik-conf:\/etc\/traefik *$/\1- traefik-conf:\/etc\/traefik:ro/g' docker-compose.yml
-docker-compose up -d
