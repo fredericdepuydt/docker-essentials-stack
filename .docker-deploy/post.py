294e026d65ce785c7c1346217bbb47ef3f89458f
Frederic Depuydt <frederic.depuydt@outlook.com>
2020-11-16 23:35:25 +0100

    Separated Authelia middleware

--- .docker-deploy/post.py
+++ .docker-deploy/post.py
@@ -9,5 +9,6 @@
 from depuydt import docker
 
 # Installing and copying files to volume
+docker.Compose.exec("traefik","","rm -r /etc/traefik/toml")
 docker.cp("config/traefik/traefik.toml","traefik:/etc/traefik")
-docker.cp("config/traefik/toml/","traefik:/etc/traefik")
+docker.cp("config/traefik/toml","traefik:/etc/traefik")
